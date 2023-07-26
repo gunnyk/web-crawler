63951db734095dc229009b778a8ff8c9c2432468
gunnyk <gunnn.kim@gmail.com>
2023-07-27 01:09:58 +0900

    feat: xlsx 파일 파싱

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const parse = require('csv-parse/lib/sync');
 const fs = require('fs');
+const xlsx = require('xlsx');
 
 const csv = fs.readFileSync('csv/data.csv');
 const records = parse(csv.toString('utf-8'));
 
-records.forEach((r, i) => {
-  console.log(i, r);
-});
+// records.forEach((r, i) => {
+//   console.log(i, r);
+// });
+
+const workbook = xlsx.readFile('xlsx/data.xlsx');
+const ws = workbook.Sheets.영화목록;
+
+const xlsxRecords = xlsx.utils.sheet_to_json(ws);
+for (const [i, r] of xlsxRecords.entries()) {
+  console.log(i, r.제목, r.링크);
+}
+
+// xlsxRecords.forEach((r) => {
+//   console.log(r.제목, r.링크);
+// });
